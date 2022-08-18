228229e9f95a3830b4ad008238b2730fcbd0f6ec
Nick Mason <nickmason372@yahoo.com>
2022-08-17 21:32:22 -0400

    Commit

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,14 @@
 var timeDisplayEl = $('#showTime');
-var storebutton1 = document.getElementById("button-addon1")
-var storebutton2 = document.getElementById("button-addon2")
-var storebutton3 = document.getElementById("button-addon3")
-var storebutton4 =document.getElementById("button-addon4")
-var storebutton5 =document.getElementById("button-addon5")
-var storebutton6 =document.getElementById("button-addon6")
-var storebutton7 = document.getElementById("button-addon7")
-var storebutton8 = document.getElementById("button-addon8")
-var storebutton9 = document.getElementById("button-addon9")
+
+var storebutton1 = document.getElementById("button-addon1");
+var storebutton2 = document.getElementById("button-addon2");
+var storebutton3 = document.getElementById("button-addon3");
+var storebutton4 =document.getElementById("button-addon4");
+var storebutton5 =document.getElementById("button-addon5");
+var storebutton6 =document.getElementById("button-addon6");
+var storebutton7 = document.getElementById("button-addon7");
+var storebutton8 = document.getElementById("button-addon8");
+var storebutton9 = document.getElementById("button-addon9");
 
 
 function displayTime() {
@@ -17,19 +18,40 @@ function displayTime() {
 
 setInterval(displayTime, 1000);
 
+function timeColors() {
+    var 
+}
+
 function saveuserInput () {
     var userInput = document.querySelector(".schedInput");
-    userInput.textContent = ("")
+    userInput.textContent= ("")
 
-    localStorage.setItem("userInput",JSON.stringify(userInput));
+    localStorage.setItem("userInput",JSON.stringify(userInput.value));
     
 }
 
+// function renderuserInput () {
+    // var lastuserInput = JSON.parse(localStorage.getItem("userInput"));
+    // if(lastuserInput !== null) {
+        // document.querySelector(".schedInput").innerHTML = lastuserInput.value;
+    // }else{
+        // return;
+    // }
+// }
+
+  
+
+function init() {
+    renderuserInput ();
+    
+}
+init ();
 
 
 storebutton1.addEventListener("click", function(event) {
 event.preventDefault();
 saveuserInput();
+renderuserInput();
 console.log(localStorage)
 })
 
