903b5c0a9b3d3a6a2c949bfa4d709ee3415b1cdd
Nick Mason <nickmason372@yahoo.com>
2022-08-18 13:47:26 -1200

    Final Commit

--- Assets/script.js
+++ Assets/script.js
@@ -1,55 +1,44 @@
+//created variables for my time and date, my table to store the inputs,the buttons for saving inputs, and the current hour 
 var timeDisplayEl = $('#showTime');
-
-var storebutton1 = document.getElementById("button-addon1");
-var storebutton2 = document.getElementById("button-addon2");
-var storebutton3 = document.getElementById("button-addon3");
-var storebutton4 =document.getElementById("button-addon4");
-var storebutton5 =document.getElementById("button-addon5");
-var storebutton6 =document.getElementById("button-addon6");
-var storebutton7 = document.getElementById("button-addon7");
-var storebutton8 = document.getElementById("button-addon8");
-var storebutton9 = document.getElementById("button-addon9");
-
-
+var table = document.querySelector(".table");
+var btn = $(".btn");
+var currentHour = moment().hour();
+//this function displays the time in the timeDisplayEl in the jumbotron
 function displayTime() {
-    var currentTime = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+    var currentTime = moment().format('MMM DD, YYYY');
     timeDisplayEl.text(currentTime);
 }
+displayTime();
+//this function grabs our stored items from Localstorage and allows them to persist on screen when refreshed as well as color codes the input elements to display different colors dependent on the current hour
+function colortimeBlock () {
+    for(var i=9; i<18; i++){
+        var text = localStorage.getItem(i)
+        $('#'+i).val(text);
+        if(currentHour > i){
+            $('#'+i).addClass("past");
+        }
+        if(currentHour === i){
+            $('#'+i).addClass("present");
+        }
+        if(currentHour < i){
+            $('#'+i).addClass("future");
+        }
+    }
+} console.log("test")
+colortimeBlock();
 
-setInterval(displayTime, 1000);
 
-function saveuserInput () {
-    var userInput = document.querySelector(".schedInput");
-    userInput.textContent= ("")
-
-    localStorage.setItem("userInput",JSON.stringify(userInput.value));
-    
+//this function saves the users inputs into the localstroage for retrieval 
+function saveuserInput(){
+    var id =$(this).attr("data-id");
+    var text =$("#"+id);
+    localStorage.setItem(id, text.val());
 }
-
- function renderuserInput () {
-     var lastuserInput = JSON.parse(localStorage.getItem("userInput"));
-     if(lastuserInput !== null) {
-         document.querySelector(".schedInput").innerHTML = lastuserInput.value;
-     }else{
-         return;
-     }
- }
-
-  
-
-function init() {
-    renderuserInput ();
-    
-}
-init ();
+btn.on("click",saveuserInput);
 
 
-storebutton1.addEventListener("click", function(event) {
-event.preventDefault();
-saveuserInput();
-renderuserInput();
-console.log(localStorage)
-})
+
+
 
 
 
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Work-Day-Scheduler
+# Description
+I created a simple app that allows the user to keep track of important events during their work day 
+
+This app will allow the user to input information in the itinerary sectiob of the table and then save it using the corresponding button 
+
+# Uses
+When the application is opened the user is presented with the current date as well as the table for them to store their daily tasks 
+
+the table is broken up into rows which corresponding to the 9-5 work schedule, an input field to into information, and a button to save that information
+
+When the user clicks into the input field they will be able to enter their information 
+
+The user can then click onto the save button to save this information to Localstorage 
+
+# Features
+
+The table uses Javascript to color code the timeblocks to represent past,present, and future events for the user. 
+
+Clicking the save button saves the event in the corresponding input field and allows those fields to persist even between refreshes 
+
+
